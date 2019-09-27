9c08e61a1722e9dfc9b902b1291f87b4635364b2
Jonas Muilwijk <jonasmuilwijk@gmail.com>
2019-09-27 10:02:18 +0200

    minor tweaks

--- index.php
+++ index.php
@@ -1,116 +1,128 @@
 <!DOCTYPE html>
-
 <html lang="en">
-  <head>
-    <title>MTG Tier List</title>
-    <meta name="description" content="MTG Tier List">
-    <meta name="author" content="theUgluk">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <title>MTG Tier List</title>
+        <meta name="description" content="MTG Tier List">
+        <meta name="author" content="theUgluk">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <style>
-      html, body {
-        margin: 0;
-        padding: 0;
-        overflow: auto;
-        position: relative;
-        height: 100%;
-        width: 100%;
-        box-sizing: border-box;
-      }
-      
-      .searchExplainer{
-          color: grey;
-      }
-      
-      #search {
-        width: 95%;
-        height: 30px;
-        font-size: 30px;
-        border: 2px solid blue;
-      }
-      #searchSubmit{
-          width: 4%;
-          height: 36px;
-          background-color: blue;
-          font-size: 20px;
-          color: white;
-          border: 2px solid blue;
-          vertical-align: top;
-          border-radius: 0 5px 5px 0;
-      }
+        <style>
+            html, body {
+                margin: 0;
+                padding: 0;
+                overflow: auto;
+                position: relative;
+                height: 100%;
+                width: 100%;
+                box-sizing: border-box;
+                /* Allow smoothe scrolling iOS */
+                -webkit-overflow-scrolling: touch;
+            }
 
-      .card-container {
-        flex: 0 1 100%;
-        margin: 0.5rem;
-        /* padding: 0.5rem; */
-        background-color: #fff;
-        /* border-bottom: 4px solid #aaa; */
-        border-radius: 15px;
-        overflow: hidden;
-        box-shadow: 0px 3px 5px rgba(0,0,0,0.5);
-        transition: all, 0.5s;
-      }
+            .searchExplainer{
+                color: grey;
+            }
 
-      .card-container img {
-        width: 100%;
-        height: auto;
-      }
+            #search {
+                width: 95%;
+                height: 30px;
+                font-size: 30px;
+                border: 2px solid blue;
+            }
+            #searchSubmit{
+                width: 4%;
+                height: 36px;
+                background-color: blue;
+                font-size: 20px;
+                color: white;
+                border: 2px solid blue;
+                vertical-align: top;
+                border-radius: 0 5px 5px 0;
+            }
 
-      .card-container .info {
-        padding: 0.5rem;
-        font-family: Arial, Helvetica, sans-serif;
-      }
+            .card-container {
+                flex: 0 1 100%;
+                margin: 0.5rem;
+                /* padding: 0.5rem; */
+                background-color: #fff;
+                /* border-bottom: 4px solid #aaa; */
+                border-radius: 15px;
+                overflow: hidden;
+                box-shadow: 0px 3px 5px rgba(0,0,0,0.5);
+                transition: all, 0.5s;
+            }
 
-      table tr td:first-of-type {
-        font-weight: 600;
-      }
+            .card-container img {
+                width: 100%;
+                height: auto;
+            }
 
-      .btn {
-        padding: 0.7rem 2rem;
-        background: orange;
-        color: white;
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 0.75em;
-        letter-spacing: 0.1em;
-        box-shadow: 0px 2px 5px rgba(0,0,0,0.2);
-        border-radius: 25px;
-        font-weight: 600;
-        text-transform: uppercase;
-        border: none;
-        margin: 1rem;
-        transition: all 0.5s;
-      }
+            .card-container .info {
+                padding: 0.5rem;
+                font-family: Arial, Helvetica, sans-serif;
+            }
 
-      .btn:hover {
-        cursor: pointer;
-        background-color: #000;
-        transition: all 0.5s;
-      }
+            table tr td:first-of-type {
+                font-weight: 600;
+            }
 
-      @media screen and (min-width: 500px) {
-        .card-container {
-          flex: 0 0 45%;
-        }
-      }
+            .btn {
+                padding: 0.7rem 2rem;
+                background: orange;
+                color: white;
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 0.75em;
+                letter-spacing: 0.1em;
+                box-shadow: 0px 2px 5px rgba(0,0,0,0.2);
+                border-radius: 25px;
+                font-weight: 600;
+                text-transform: uppercase;
+                border: none;
+                margin: 1rem;
+                transition: all 0.5s;
+            }
 
-      @media screen and (min-width: 800px) {
-        .card-container {
-          flex: 0 0 20%;
-        }
-      }
+            .btn:hover {
+                cursor: pointer;
+                background-color: #000;
+                transition: all 0.5s;
+            }
 
-      @media screen and (min-width: 1500px) {
-        .card-container {
-          flex: 0 0 15%;
-        }
-      }
+            @media screen and (max-width: 800px) {
+                #search{
+                    /* Omit border (mss andere border-box ofzo?) */
+                    width: calc(100% - 6px);
+                }
+                #searchSubmit{
+                    width: 100%;
+                    text-align: center;
+                    border-radius: 0px;
+                }
+            }
+            @media screen and (min-width: 500px) {
+                .card-container {
+                    flex: 0 0 45%;
+                }
+            }
 
-    </style>
-  </head>
+            @media screen and (min-width: 800px) {
+                .card-container {
+                    flex: 0 0 20%;
+                }
+            }
 
-  <body>
+            @media screen and (min-width: 1500px) {
+                .card-container {
+                    flex: 0 0 15%;
+                }
+            }
 
-    <?php
+        </style>
+    </head>
+
+    <body>
+
+        <?php
         require 'setup.php';
         require 'functions.php';
 
@@ -124,7 +136,7 @@
             "sorcery"
         );
         $foundNames = array();
-        if(isset($_GET['query'])){
+        if (isset($_GET['query'])) {
             $foundNames = search_query($_GET['query']);
         } else {
             /*
@@ -132,7 +144,7 @@
              * selected and filter them in the while. It's ugly as hell,
              * but what in this script ain't?
              */
-            if(isset($_GET['type']) && in_array($_GET['type'], $types, true)){
+            if (isset($_GET['type']) && in_array($_GET['type'], $types, true)) {
                 $foundNames = get_type_data($_GET['type']);
             }
         }
@@ -145,28 +157,28 @@
         $apiUrl = "https://api.scryfall.com/";
         $image = 'large';
 
-        foreach($types as $type){
+        foreach ($types as $type) {
             echo '<a href="?type=' . $type . '"><button class="btn">' . ucfirst($type) . '</button></a>';
         }
-    ?>
-    <br />
-    <form method="get">
-        <input type="text" id="search" placeholder="Search" name="query" value="<?= isset($_GET['query']) ? $_GET['query'] : "" ?>" /><input type="submit" id="searchSubmit" value="search" /><br />
-        <small class="searchExplainer">
-            You can use Scryfall query's here
-        </small>
-    </form>
-    <div style="display: flex; max-width: 100vw; flex-wrap: wrap; background-color: #baeaf7;">
-        <?php
+        ?>
+        <br />
+        <form method="get">
+            <input type="text" id="search" placeholder="Search" name="query" value="<?= isset($_GET['query']) ? $_GET['query'] : "" ?>" /><input type="submit" id="searchSubmit" value="search" /><br />
+            <small class="searchExplainer">
+                You can use Scryfall query's here
+            </small>
+        </form>
+        <div style="display: flex; max-width: 100vw; flex-wrap: wrap; background-color: #baeaf7;">
+            <?php
             while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
                 if ($row[2] !== "") {
                     $clearName = str_replace("//", "", $row[2]);
-                    if(is_array($foundNames)
-                        && (
+                    if (is_array($foundNames)
+                            && (
                             count($foundNames) === 0
                             || in_array($clearName, $foundNames)
-                        )
-                    ){
+                            )
+                    ) {
                         $cardName = urlencode($row[2]);
                         $img = "imgs/" . $clearName . ".jpg";
                         if (!file_exists($img)) {
@@ -225,11 +237,11 @@
                                 </table>
                             </div>
                         </div>
-                    <?php
+                        <?php
                     }
                 }
             }
-        ?>
-    </div>
-  </body>
+            ?>
+        </div>
+    </body>
 </html>
