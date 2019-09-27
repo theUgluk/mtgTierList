b396af7170a47142776b1e7e4dabfb983e771066
Jaimy <jaimyrijnders@gmail.com>
2019-09-27 21:29:07 +0200

    Updated search and filters

--- index.php
+++ index.php
@@ -6,117 +6,8 @@
         <meta name="author" content="theUgluk">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <style>
-            html, body {
-                margin: 0;
-                padding: 0;
-                overflow: auto;
-                position: relative;
-                height: 100%;
-                width: 100%;
-                box-sizing: border-box;
-                /* Allow smoothe scrolling iOS */
-                -webkit-overflow-scrolling: touch;
-            }
+        <link href="styles.css" type="text/css" rel="stylesheet">
 
-            .searchExplainer{
-                color: grey;
-            }
-
-            #search {
-                width: 95%;
-                height: 30px;
-                font-size: 30px;
-                border: 2px solid blue;
-            }
-            #searchSubmit{
-                width: 4%;
-                height: 36px;
-                background-color: blue;
-                font-size: 20px;
-                color: white;
-                border: 2px solid blue;
-                vertical-align: top;
-                border-radius: 0 5px 5px 0;
-            }
-
-            .card-container {
-                flex: 0 0 250px;
-                justify-content: space-evenly;
-                margin: 0.5rem;
-                background-color: #fff;
-                border-radius: 15px;
-                overflow: hidden;
-                box-shadow: 0px 3px 5px rgba(0,0,0,0.5);
-                transition: all, 0.5s;
-            }
-
-            .card-container img {
-                width: 100%;
-                height: auto;
-            }
-
-            .card-container .info {
-                padding: 0.5rem;
-                font-family: Arial, Helvetica, sans-serif;
-            }
-
-            table tr td:first-of-type {
-                font-weight: 600;
-            }
-
-            .btn {
-                padding: 0.7rem 2rem;
-                background: orange;
-                color: white;
-                font-family: Arial, Helvetica, sans-serif;
-                font-size: 0.75em;
-                letter-spacing: 0.1em;
-                box-shadow: 0px 2px 5px rgba(0,0,0,0.2);
-                border-radius: 25px;
-                font-weight: 600;
-                text-transform: uppercase;
-                border: none;
-                margin: 1rem;
-                transition: all 0.5s;
-            }
-
-            .btn:hover {
-                cursor: pointer;
-                background-color: #000;
-                transition: all 0.5s;
-            }
-
-            @media screen and (max-width: 800px) {
-                #search{
-                    /* Omit border (mss andere border-box ofzo?) */
-                    width: calc(100% - 6px);
-                }
-                #searchSubmit{
-                    width: 100%;
-                    text-align: center;
-                    border-radius: 0px;
-                }
-            }
-            @media screen and (min-width: 500px) {
-                /* .card-container {
-                    flex: 0 0 45%;
-                } */
-            }
-
-            @media screen and (min-width: 800px) {
-                /* .card-container {
-                    flex: 0 0 20%;
-                } */
-            }
-
-            @media screen and (min-width: 1500px) {
-                /* .card-container {
-                    flex: 0 0 15%;
-                } */
-            }
-
-        </style>
     </head>
 
     <body>
@@ -156,91 +47,119 @@
         $apiUrl = "https://api.scryfall.com/";
         $image = 'large';
 
-        foreach ($types as $type) {
-            echo '<a href="?type=' . $type . '"><button class="btn">' . ucfirst($type) . '</button></a>';
-        }
+
         ?>
         <br />
-        <form method="get">
-            <input type="text" id="search" placeholder="Search" name="query" value="<?= isset($_GET['query']) ? $_GET['query'] : "" ?>" /><input type="submit" id="searchSubmit" value="search" /><br />
-            <small class="searchExplainer">
-                You can use Scryfall query's here
-            </small>
-        </form>
-        <div style="display: flex; max-width: 100vw; flex-wrap: wrap; background-color: #baeaf7;">
-            <?php
-            while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
-                if ($row[2] !== "") {
-                    $clearName = str_replace("//", "", $row[2]);
-                    if (is_array($foundNames)
-                            && (
-                            count($foundNames) === 0
-                            || in_array($clearName, $foundNames)
-                            )
-                    ) {
-                        $cardName = urlencode($row[2]);
-                        $img = "imgs/" . $clearName . ".jpg";
-                        if (!file_exists($img)) {
-                            $cardFind = file_get_contents($apiUrl . "cards/named?format=image&version=large&exact=" . $cardName);
-                            file_put_contents($img, $cardFind);
-                        }
-                        ?>
-                        <div class="card-container">
-                            <img src="<?= $img ?>" />
-                            <div class="info">
-                                <table>
-                                    <tr>
-                                        <td>
-                                            J's mark:
-                                        </td>
-                                        <td>
-                                            <?= $row[0] ?>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            M's mark:
-                                        </td>
-                                        <td>
-                                            <?= $row[1] ?>
-                                        </td>
-                                    </tr>
-                                    <?php
-                                    if ($row[4] !== "") {
-                                        ?>
-                                        <tr>
-                                            <td>
-                                                Ceiling:
-                                            </td>
-                                            <td>
-                                                <?= $row[4] ?>
-                                            </td>
-                                        </tr>
-                                        <?php
-                                    }
-                                    ?>
-                                    <?php
-                                    if ($row[5] !== "") {
-                                        ?>
-                                        <tr>
-                                            <td>
-                                                Sideboard:
-                                            </td>
-                                            <td>
-                                                <?= $row[5] ?>
-                                            </td>
-                                        </tr>
-                                        <?php
-                                    }
-                                    ?>
-                                </table>
-                            </div>
-                        </div>
-                        <?php
-                    }
-                }
-            }
-            ?>
+
+
+        <div class="wrapper">
+          <div class="flex-container">
+            <aside class="sidebar">
+              <div class="sidebar-wrapper">
+                <h3>Filter cards</h3>
+                <div class="sidebar-module">
+                  <form method="get">
+                      <input type="text" id="search" placeholder="Search" name="query" value="<?= isset($_GET['query']) ? $_GET['query'] : "" ?>" />
+                      <small class="searchExplainer">
+                          You can use Scryfall query's here
+                      </small>
+                      <input type="submit" id="searchSubmit" class="btn" value="search" />
+                  </form>
+                </div>
+                <div class="sidebar-module">
+                  <!-- <span class="module-title">Show only:</span> -->
+                  <?php
+                  foreach ($types as $type) {
+                      // echo '<a href="?type=' . $type . '"><button class="btn">' . ucfirst($type) . '</button></a>';
+                      // echo '<div><a href="?type=' . $type . '"><input type="radio" id="' . $type .'" name="card-type"><label for="' . $type .'">' . ucfirst($type) . '</label></a></div>';
+                      echo '<a href="?type=' . $type . '" class="sidebar-link">' . ucfirst($type) . '</a>';
+                  } ?>
+                </div>
+
+              </div>
+            </aside>
+
+            <main class="main-content">
+              <div style="display: flex; max-width: 100vw; flex-wrap: wrap; justify-content: space-evenly; ">
+                  <?php
+                  while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
+                      if ($row[2] !== "") {
+                          $clearName = str_replace("//", "", $row[2]);
+                          if (is_array($foundNames)
+                                  && (
+                                  count($foundNames) === 0
+                                  || in_array($clearName, $foundNames)
+                                  )
+                          ) {
+                              $cardName = urlencode($row[2]);
+                              $img = "imgs/" . $clearName . ".jpg";
+                              if (!file_exists($img)) {
+                                  $cardFind = file_get_contents($apiUrl . "cards/named?format=image&version=large&exact=" . $cardName);
+                                  file_put_contents($img, $cardFind);
+                              }
+                              ?>
+                              <div class="card-container">
+                                  <img src="<?= $img ?>" />
+                                  <div class="info">
+                                      <table>
+                                          <tr>
+                                              <td>
+                                                  J's mark:
+                                              </td>
+                                              <td>
+                                                  <?= $row[0] ?>
+                                              </td>
+                                          </tr>
+                                          <tr>
+                                              <td>
+                                                  M's mark:
+                                              </td>
+                                              <td>
+                                                  <?= $row[1] ?>
+                                              </td>
+                                          </tr>
+                                          <?php
+                                          if ($row[4] !== "") {
+                                              ?>
+                                              <tr>
+                                                  <td>
+                                                      Ceiling:
+                                                  </td>
+                                                  <td>
+                                                      <?= $row[4] ?>
+                                                  </td>
+                                              </tr>
+                                              <?php
+                                          }
+                                          ?>
+                                          <?php
+                                          if ($row[5] !== "") {
+                                              ?>
+                                              <tr>
+                                                  <td>
+                                                      Sideboard:
+                                                  </td>
+                                                  <td>
+                                                      <?= $row[5] ?>
+                                                  </td>
+                                              </tr>
+                                              <?php
+                                          }
+                                          ?>
+                                      </table>
+                                  </div>
+                              </div>
+                              <?php
+                          }
+                      }
+                  }
+                  ?>
+              </div>
+            </main>
+
+          </div>
         </div>
+
+
     </body>
 </html>
