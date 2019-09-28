ea3d9512e867d0fcf6ab4329be9496eebbbc00f0
JaimyR <jaimyrijnders@gmail.com>
2019-09-28 17:29:26 +0200

    Added main.js and minor update to marks

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
 
         <link href="styles.css" type="text/css" rel="stylesheet">
 
+        <script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
+
     </head>
 
     <body>
